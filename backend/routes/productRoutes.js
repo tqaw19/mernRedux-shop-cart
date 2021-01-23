d3b445d91b375fa0838bb23a3ebf1e6142bbd069
tqaw19 <tqaw19@gmail.com>
2021-01-22 19:57:58 -0500

    finishing cartItems component

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,14 +3,18 @@ const router = express.Router();
 
 const { getAllProducts, getProductById } = require('../controller/productController')
 
-//@desc GET all products from DB
-//@route GET /api/products
-//@access Public
+/**
+ * @desc GET all products from DB
+ * @route GET /api/products
+ * @access Public
+ */
 router.get('/', getAllProducts)
 
-//@desc GET a product by id from DB
-//@route GET /api/products/:id
-//@access Public
+/**
+ * @desc GET a product by id from DB
+ * @route GET /api/products/:id
+ * @access Public
+ */
 router.get('/:id', getProductById)
 
 module.exports = router;
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,9 +1,24 @@
+import CartItem from '../components/CartItem'
 import './CartScreen.css'
 
 const CartScreen = () => {
     return (
         <div className="cartscreen">
-            Cart Screen
+            <div className="cartscreen__left">
+                <h2>Shopping Cart</h2>
+
+                <CartItem />
+            </div>
+
+            <div className="cartscreen__right">
+                <div className="cartscreen__info">
+                    <p>Subtotal (0) items</p>
+                    <p>$499.99</p>
+                </div>
+                <div>
+                    <button>Proceed To Checkout</button>
+                </div>
+            </div>
         </div>
     )
 }
